17c3971eaed51b758f21834bca2b908c3ed2506f
stefan-kolb <stefan-kolb@web.de>
2015-05-27 08:31:19 -0700

    Concat random number to test app name ~ closes #4

--- test/test_helper.bash
+++ test/test_helper.bash
@@ -1,7 +1,7 @@
 mk_app_dir() {
   export TMP="$BATS_TEST_DIRNAME/tmp"
   mkdir -p "$BATS_TEST_DIRNAME/tmp"
-  export APP_NAME=$(echo "$BATS_TEST_NAME" | tr -d _)
+  export APP_NAME=$(echo "${BATS_TEST_NAME}$RANDOM" | tr -d _)
   export APP_DIR="$TMP/$APP_NAME"
   mkdir -p "$APP_DIR"
 }
